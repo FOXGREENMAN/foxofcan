0ebb2e6d05e2c214c1a408ee2fd701cd06f9d099
yeyuexin <yeyuexin@chainup.com>
2020-06-03 15:31:16 +0800

    change model name

---
+++ foxofcan-common/src/main/java/com/foxofcan/common/exception/DemoModeException.java
@@ -0,0 +1,15 @@
+package com.foxofcan.common.exception;
+
+/**
+ * 演示模式异常
+ * 
+ * @author foxofcan
+ */
+public class DemoModeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException()
+    {
+    }
+}
